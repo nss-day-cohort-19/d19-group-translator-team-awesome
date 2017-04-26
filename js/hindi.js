046d8520f7e3f2c44cd1d622c7e52166037a4467
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-04-26 08:45:47 -0500

    iife

--- js/hindi.js
+++ js/hindi.js
@@ -1,28 +1,32 @@
 
+var mainTranslate = (function(hindiTranslate){
 
-function translateToHindi(text){
-	var translation = "";
-	console.log("within my function");
-	console.log(text);
-	var lexicon = {"happy":"shubh", "mothers":"mataji", "day":"din"};
-	var myArray = text.split(" ");
-	console.log("myArray",myArray);
-	console.log(lexicon.happy);
-	for(i=0;i<myArray.length;i++){
-		
-		for(var j in lexicon){
-		if(myArray[i]===j)
-		{
-			translation += lexicon[j] + " ";
-			console.log(myArray[i]);	
-			console.log(lexicon[j]);
-			console.log(j);
-			console.log("translation",translation);
+		console.log(hindiTranslate);
+		hindiTranslate.translateToHindi = function(text){
+		var translation = "";
+		console.log("within my function");
+		console.log(text);
+		var lexicon = {"happy":"shubh", "mothers":"mataji", "day":"din"};
+		var myArray = text.split(" ");
+		console.log("myArray",myArray);
+		console.log(lexicon.happy);
+		for(i=0;i<myArray.length;i++){
+			
+			for(var j in lexicon){
+				if(myArray[i]===j)
+				{
+					translation += lexicon[j] + " ";
+					console.log(myArray[i]);	
+					console.log(lexicon[j]);
+					console.log(j);
+					console.log("translation",translation);
+				}
+			}
+
+			
 		}
-	}
-
+		return translation;
 		
 	}
-	return translation;
-	
-}
+	return hindiTranslate;
+})(mainTranslate);
--- js/main.js
+++ js/main.js
@@ -1,52 +1,56 @@
 console.log("i am here");
 
+
+
+var mainTranslate = {};
+
 //get translate button element
-let translate = document.getElementById("translateBtn");
+	let translate = document.getElementById("translateBtn");
 
 
-//get output text element
-let output = document.getElementById("output");
+	//get output text element
+	let output = document.getElementById("output");
 
 
-//set event listener on button element
-translate.addEventListener("click", function(){
+	//set event listener on button element
+	translate.addEventListener("click", function(){
+		//debugger
+		//input text
+		let translatePhrase = document.getElementById("input").value;
 
-	//input text
-	let translatePhrase = document.getElementById("input").value;
+		var whichselected;
 
-	var whichselected;
-
-	//get selected language option
-	var selectLanguage = document.getElementById("languageType");
-	//console.log("selectLanguage", selectLanguage);
-	for (var i=0; i<selectLanguage.length; i++){
-		if(selectLanguage.options[selectLanguage.selectedIndex]){
-			whichselected = selectLanguage.options[selectLanguage.selectedIndex].value;
-			console.log("whichselected", whichselected);
-			break;
+		//get selected language option
+		var selectLanguage = document.getElementById("languageType");
+		//console.log("selectLanguage", selectLanguage);
+		for (var i=0; i<selectLanguage.length; i++){
+			if(selectLanguage.options[selectLanguage.selectedIndex]){
+				whichselected = selectLanguage.options[selectLanguage.selectedIndex].value;
+				console.log("whichselected", whichselected);
+				break;
+			}
 		}
-	}
 
-	//call corresponding language functions
-	if (whichselected == "hindi"){
-		console.log(whichselected);
-		var outputText = translateToHindi(translatePhrase);
-		output.innerHTML = outputText;
-	} else if(whichselected == "russian"){
-		console.log(whichselected);
-		//mainTranslate.translateToRussian(translatePhrase);
-	}else if(whichselected == "spanish"){
-		console.log(whichselected);
-		//mainTranslate.translateToSpanish(translatePhrase);
-	}else if(whichselected == "french"){
-		console.log(whichselected);
-		//mainTranslate.translateToFrench(translatePhrase);
-	}
+		//call corresponding language functions
+		if (whichselected == "hindi"){
+			console.log(whichselected);
+			var outputText = mainTranslate.translateToHindi(translatePhrase);
+			output.innerHTML = outputText;
+		} else if(whichselected == "russian"){
+			console.log(whichselected);
+			//mainTranslate.translateToRussian(translatePhrase);
+		}else if(whichselected == "spanish"){
+			console.log(whichselected);
+			//mainTranslate.translateToSpanish(translatePhrase);
+		}else if(whichselected == "french"){
+			console.log(whichselected);
+			//mainTranslate.translateToFrench(translatePhrase);
+		}
 
 
-})
-	
-	
+	});
+		
+
 
 
 
